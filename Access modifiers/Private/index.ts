ac5ee5962764a213b2d82b5faa91d4a5c2cb9d80
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-11-01 13:56:56 +0200

    private access modifier example

---
+++ Access modifiers/Private/index.ts
@@ -0,0 +1,49 @@
+/*
+    Private
+        By using private access modifier,
+        properties and methods can only be accessed by the object that owns them.
+*/
+
+/*
+    In Person, Student example we will redeclare email as a private property
+*/
+class Person {
+    // private property
+    private _email: string;
+
+    public constructor(email: string) {
+        this._email = email;
+    }
+
+    // a getter method to get the private property
+    public getEmail() {
+        return this._email;
+    }
+}
+
+class Student extends Person {
+    public id: number;
+
+    public constructor(email: string, id: number) {
+        super(email);
+        this.id = id;
+    }
+    public shareEmail(): void {
+        //console.log(`My email is ${this._email}`);
+        // This will give an error,because _email is private in Person class.
+
+        // we can use getEmail() method to get the email.
+        console.log(`My email is ${this.getEmail()}`);
+    }
+}
+
+// Person instance
+const person = new Person('osama.s.abdelmoniem@gmail.com');
+// accessing email property using Person instance
+// console.log(person._email); will give an error
+
+// Student instance
+const student = new Student('osama.s.abdelmoniem@gmail.com', 123456);
+// accessing email property using Student instance
+// console.log(student._email); will give an error
+student.shareEmail();
