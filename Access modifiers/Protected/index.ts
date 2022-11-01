c5d8b8a6ff45f5e8d6bc23991d7ee52d981f04eb
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-11-01 14:07:00 +0200

    simple protected access modifier example

---
+++ Access modifiers/Protected/index.ts
@@ -0,0 +1,38 @@
+/*
+    Protected
+        By using protected access modifier,
+        properties and methods can only be accessed by the object that owns them
+        or instances of the derived classes.
+*/
+
+/*
+    In Person, Student example we will redeclare email as a protected property
+*/
+
+class Person {
+    // protected property
+    protected _email: string;
+
+    public constructor(email: string) {
+        this._email = email;
+    }
+}
+
+class Student extends Person {
+    public shareEmail(): void {
+        // accessing _email protected property and it works.
+        console.log(this._email);
+    }
+}
+
+/*
+    If we create instance of Person and Student classes,
+    _email can be access by shareEmail() method.
+    We cant access _email with the object because its protected.
+*/
+const person = new Person('osama.s.abdelmoniem@gmail.com');
+// console.log(person._email); will give an error
+
+const student = new Student('osama.s.abdelmoniem@gmail.com');
+// console.log(student._email) will give an error
+student.shareEmail();
